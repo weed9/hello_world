f6630c86f34822196b031fea26ffc19056858640
kang <kangyr08@gmail.com>
2016-01-20 14:40:59 +0800

    Just add two new functions. Please enter the commit message for your changes. Lines starting

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+int add(int a, int b){
+	return a+b;
+}
+
+template<class T> void  print(vector<T> v){
+	cout<<"{"<<endl;
+	for(T t: v)
+		cout<<t<<endl;
+	cout<<"}"<<endl;
+}
 
 int main(){
 	cout<<"Hello World!"<<endl;
